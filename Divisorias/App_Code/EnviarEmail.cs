058165bb624554ffb3e8cd4991e2dab2f82192c0
Michelle <nogueira.michelle@gmail.com>
2020-07-12 19:40:26 -0300

    contato

---
+++ Divisorias/App_Code/EnviarEmail.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+public class EnviaEmail
+{
+    public static string EnviarEmail(string EmailTo, string EmailReplyTo, string Assunto, string Mensagem)
+    {
+
+        string retorno = DBNull.Value.ToString();
+
+        // Configurações do Email
+        MailMessage mail = new MailMessage();
+        mail.From = new MailAddress(EmailReplyTo + "<sistema@diapoio.com.br>");
+        mail.To.Add(FormatMultipleEmailAddresses(EmailTo));
+        if (!string.IsNullOrEmpty(EmailReplyTo))
+        {
+            mail.ReplyTo = new MailAddress(EmailReplyTo);
+        }
+        mail.IsBodyHtml = true;
+        //Define Prioridade
+        mail.Priority = MailPriority.Normal;
+        //Determina a codificação da mensagem no padrão brasileiro
+        mail.BodyEncoding = System.Text.Encoding.GetEncoding("ISO-8859-1");
+        //Determina a codificação do assunto da mensagem no padrão brasileito
+        mail.SubjectEncoding = System.Text.Encoding.GetEncoding("ISO-8859-1");
+        //Assunto do email
+        mail.Subject = Assunto;
+        //Define o conteúdo da mensagem
+        mail.Body = Mensagem;
+
+        //Instancia o objeto de envio da mensagem
+        SmtpClient smtp = new SmtpClient();
+        //Determina o servidor de envio da mensagem
+        smtp.Host = "smtp.zoho.com";
+        //Determina a porta do servidor SMTP
+        smtp.Port = Convert.ToInt16("587");
+        //Credencial
+        smtp.UseDefaultCredentials = false;
+        //Instancia o objeto que permite fazer autenticação no envio de e-mails via SMTP
+        smtp.Credentials = new System.Net.NetworkCredential("sistema@diapoio.com.br", "Did!sed3@123");
+        //SSL
+        smtp.EnableSsl = true;
+
+        try
+        {
+            //Envia a mensagem
+            smtp.Send(mail);
+        }
+        catch (Exception ex)
+        {
+            retorno = ex.Message.ToString();
+        }
+        mail.Dispose();
+        return retorno;
+    }
+
+    public static string FormatMultipleEmailAddresses(string emailAddresses)
+    {
+        var delimiters = new[] { ',', ';' };
+
+        var addresses = emailAddresses.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+        return string.Join(",", addresses);
+    }
+}
--- Divisorias/Contato.aspx.cs
+++ Divisorias/Contato.aspx.cs
@@ -18,68 +18,42 @@ public partial class Contato : System.Web.UI.Page
     {
         if (Page.IsValid)
         {
-            //Instancia o objeto que monta a mensagem já passando como parâmetro o e-mail do remetente e destinatário
-            MailMessage mail = new MailMessage(txtEmail.Text, "contato@lusole.com.br");
-
-            //Define o CC(Com Cópia) e CCO(Com cópia oculta) caso tenha sido informado
-            //mail.CC.Add("contato@apasbraganca.com.br");
-            //mail.Bcc.Add("contato@apasbraganca.com.br");
-
-            //Define o asunto da mensagem
-            mail.Subject = txtAssunto.Text;
-
-
-            //Define o conteúdo da mensagem
-            StringBuilder message = new StringBuilder();
-
-            message.Append("<b>Visitante Contato</b>" + "<br>");
-            message.Append("========================================" + "<br>");
-            message.Append("<b>Nome:</b> " + txtNome.Text + "<br>");
-            message.Append("<b>Email:</b> " + txtEmail.Text + "<br>");
-            message.Append("<b>Telefone:</b> " + txtTelefone.Text + "<br>");
-            message.Append("<b>Assunto:</b> " + txtAssunto.Text + "<br>");
-            message.Append("<b>Descrição:</b> " + txtMessage.Text + "<br>");
-            message.Append("========================================");
-
-            mail.Body = message.ToString();
-            mail.IsBodyHtml = true;
-            mail.Priority = MailPriority.Normal;
-
-            //Determina a codificação da mensagem no padrão brasileiro
-            mail.BodyEncoding = System.Text.Encoding.GetEncoding("ISO-8859-1");
-
-            //Determina a codificação do assunto da mensagem no padrão brasileito
-            mail.SubjectEncoding = System.Text.Encoding.GetEncoding("ISO-8859-1");
-
-            //Instancia o objeto de envio da mensagem
-            SmtpClient smtp = new SmtpClient();
-
-            //Determina o servidor de envio da mensagem
-            smtp.Host = "mail.lusole.com.br";
-
-            //Determina a porta do servidor SMTP
-            smtp.Port = Convert.ToInt16("587");
-
-            //Instancia o objeto que permite fazer autenticação no envio de e-mails via SMTP
-            smtp.Credentials = new System.Net.NetworkCredential("contato@lusole.com.br", "Didisede123");
-
             try
             {
-                //Envia a mensagem
-                smtp.Send(mail);
-                LblMessage.Text = "Mensagem enviada com sucesso!";
-                txtNome.Text = string.Empty;
-                txtEmail.Text = string.Empty;
-                txtTelefone.Text = string.Empty;
-                txtAssunto.Text = string.Empty;
-                txtMessage.Text = string.Empty;
-                btnEnviar.Enabled = false;
+                string resultado = DBNull.Value.ToString();
+
+                //Define o conteúdo da mensagem
+                StringBuilder message = new StringBuilder();
+
+
+                message.Append("<b>Visitante Contato</b>" + "<br>");
+                message.Append("========================================" + "<br>");
+                message.Append("<b>Nome:</b> " + txtNome.Text + "<br>");
+                message.Append("<b>Email:</b> " + txtEmail.Text + "<br>");
+                message.Append("<b>Telefone:</b> " + txtTelefone.Text + "<br>");
+                message.Append("<b>Assunto:</b> " + txtAssunto.Text + "<br>");
+                message.Append("<b>Descrição:</b> " + txtMessage.Text + "<br>");
+                message.Append("========================================");
+
+                resultado = EnviaEmail.EnviarEmail("lusolem@yahoo.com.br", txtEmail.Text, "Contato Site", message.ToString());
+
+                if (String.IsNullOrEmpty(resultado))
+                {
+                    LblMessage.Text = "Mensagem enviada com sucesso!";
+                    txtNome.Text = string.Empty;
+                    txtEmail.Text = string.Empty;
+                    txtTelefone.Text = string.Empty;
+                    txtAssunto.Text = string.Empty;
+                    txtMessage.Text = string.Empty;
+                    btnEnviar.Enabled = false;
+                }
+
             }
             catch (Exception ex)
             {
+
                 LblMessage.Text = ex.Message;
             }
-            mail.Dispose();
         }
         else
         {
